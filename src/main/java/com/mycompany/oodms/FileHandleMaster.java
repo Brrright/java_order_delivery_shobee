452fd71e0df8325e9076f9bff26d001ec89ba475
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-01-02 15:53:30 +0800

    Initial commit
    for sharing files

---
+++ src/main/java/com/mycompany/oodms/FileHandleMaster.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oodms;
+import java.io.File;
+
+/**
+ *
+ * @author mingl
+ */
+public class FileHandleMaster {
+    // create new file
+
+    // fetch data
+    
+    // insert data
+    
+    // update data
+    
+    // delete data
+    
+}
