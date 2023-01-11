2d2add25613dce622526faf9c5a4bedbe4cbc6bf
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-01-11 23:18:55 +0800

    Create Address.java

---
+++ src/main/java/com/mycompany/oodms/Address.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oodms;
+
+/**
+ *
+ * @author mingl
+ */
+
+enum PostCode{
+//    "51600",
+    
+}
+
+public class Address {
+    
+}
