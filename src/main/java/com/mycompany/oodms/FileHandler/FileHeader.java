32096ccafddac2259d276d6eb3037a76c83560ef
Brrright <mingliang0524@gmail.com>
2023-01-18 18:36:10 +0800

    WIP ui cotroller ad file header

--- src/main/java/com/mycompany/oodms/FileHandler/FileHeader.java
+++ src/main/java/com/mycompany/oodms/FileHandler/FileHeader.java
@@ -9,5 +9,9 @@ package com.mycompany.oodms.FileHandler;
  * @author mingl
  */
 public class FileHeader {
-    
+    final static String admin = "AdminID;Admin Name;Admin Email;Password;Age;Gender;Phone Number;Picture";
+    final static String cart = "CartID;CustID;ProductID;Quantity";
+    final static String category = "CategoryID;CategoryName";
+    final static String delivery = "DeliveryID;Status;Date;AddressID";
+    final static String delivery_staff = "StaffID;Staff Name;Staff Email;Password;Age;Gender;State;Phone Number;Picture";
 }
