c255284a15d945d1aafda0a25e5576e7a723f25f
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-01-19 16:41:12 +0800

    Update FileHeader.java

--- src/main/java/com/mycompany/oodms/FileHandler/FileHeader.java
+++ src/main/java/com/mycompany/oodms/FileHandler/FileHeader.java
@@ -10,8 +10,18 @@ package com.mycompany.oodms.FileHandler;
  */
 public class FileHeader {
     final static String admin = "AdminID;Admin Name;Admin Email;Password;Age;Gender;Phone Number;Picture";
-    final static String cart = "CartID;CustID;ProductID;Quantity";
-    final static String category = "CategoryID;CategoryName";
-    final static String delivery = "DeliveryID;Status;Date;AddressID";
     final static String delivery_staff = "StaffID;Staff Name;Staff Email;Password;Age;Gender;State;Phone Number;Picture";
+    final static String member = "MemberID;Member Name;Member Email;Password;Age;Gender;Phone Number;Picture";
+
+    final static String product = "ProductID;Product Name;Price;Stock Quantity;Picture;CategoryID";
+    final static String category = "CategoryID;CategoryName";
+    
+    final static String warehouse = "Name;Street Name;City;State;Postcode";
+    final static String delivery = "DeliveryID;Status;Date;AddressID";
+    
+    final static String member_address = "AddressID;MemberID;StreetName;City;State;Postcode";
+    final static String cart = "CartID;CustID;ProductID;Quantity";
+    final static String order = "OrderID;Date;DeliveryAddress;TotalPrice;Paid;Change";
+    final static String order_item = "ProductID;Quantity;OrderID";
+    
 }
