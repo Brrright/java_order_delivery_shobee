0b2ab45aabfaf8244d9f558fb1ee073017e444e1
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-01-27 11:28:50 +0800

    created cart and order class

---
+++ src/main/java/com/mycompany/oodms/Cart.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oodms;
+
+/**
+ *
+ * @author mingl
+ */
+class Cart {
+    private String cartID;
+    private Product product;
+    private int quantity;
+    
+    Cart(String id){
+        this.cartID = id;
+    }
+    
+    Cart(String id, int quantity){
+        this.cartID = id;
+        this.product = new Product();//?
+        this.quantity = quantity;
+    }
+    
+    
+    public String getCartID(){
+        return this.cartID;
+    }
+    
+    public int getQuantity(){
+        return this.quantity;
+    }
+}
---
+++ src/main/java/com/mycompany/oodms/Order.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oodms;
+
+
+/**
+ *
+ * @author mingl
+ */
+public class Order {
+    private String orderID;
+    private String date_time;
+    private String addressID;
+    private double totalPrice;
+    private double paid;
+    private double change;
+    
+    public String getOrderID() {
+        return this.orderID;
+    }
+
+
+    
+}
