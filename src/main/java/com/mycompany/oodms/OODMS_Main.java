f75d219a41e7e074f945b147e3cb5817266ebe98
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-02-03 16:58:46 +0800

    WIP oodms template

--- src/main/java/com/mycompany/oodms/OODMS_Main.java
+++ src/main/java/com/mycompany/oodms/OODMS_Main.java
@@ -4,7 +4,9 @@
  */
 package com.mycompany.oodms;
 
+import com.company.ui.Login;
 import com.mycompany.oodms.ui.Main_Frame;
+import javax.swing.JPanel;
 
 /**
  *
@@ -14,7 +16,16 @@ public class OODMS_Main {
     public static Main_Frame frame;
     public static User current_user;
     
-    public void initialize(){
-        // setup files
+     public void initialize(){
+        // setup 
+//        this.frame = new Main_Frame(new JPanel);
+        new Login();
+        
+    }
+    
+    public static void main(String[] args) {
+//        initialize();
+        OODMS_Main main = new OODMS_Main();
+        main.initialize();
     }
 }
