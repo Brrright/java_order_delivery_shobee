15b7124d25010250e3dd5bffbfe34a622add4949
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-02-10 20:55:01 +0800

    Updated FileHeader & Created static FileName

--- src/main/java/com/mycompany/oodms/FileRelatedClass/FileHeader.java
+++ src/main/java/com/mycompany/oodms/FileRelatedClass/FileHeader.java
@@ -9,18 +9,18 @@ package com.mycompany.oodms.FileRelatedClass;
  * @author mingl
  */
 public class FileHeader {
-    final static String admin = "AdminID;Admin Name;Admin Email;Password;Age;Gender;Phone Number;Picture";
-    final static String delivery_staff = "StaffID;Staff Name;Staff Email;Password;Age;Gender;State;Phone Number;Picture";
-    final static String member = "MemberID;Member Name;Member Email;Password;Age;Gender;Phone Number;Picture";
+    final static String ADMIN_HEADER = "AdminID;Admin Name;Admin Email;Password;Age;Gender;Phone Number;Picture";
+    final static String DELIVERY_STAFF_HEADER = "StaffID;Staff Name;Staff Email;Password;Age;Gender;State;Phone Number;Picture";
+    final static String MEMBER_HEARDER = "MemberID;Member Name;Member Email;Password;Age;Gender;Phone Number;Picture";
 
-    final static String product = "ProductID;Product Name;Price;Stock Quantity;Picture;CategoryID";
-    final static String category = "CategoryID;CategoryName";
+    final static String PRODUCT_HEADER = "ProductID;Product Name;Price;Stock Quantity;Picture;CategoryID";
+    final static String CATEGORY_HEADER = "CategoryID;CategoryName";
     
-    final static String warehouse = "Name;Street Name;City;State;Postcode";
-    final static String delivery = "DeliveryID;Status;Date;AddressID";
+    final static String WAREHOUSE_HEADER = "Name;Street Name;City;State;Postcode";
+    final static String DELIVERY_HEADER = "DeliveryID;Status;Date;AddressID";
     
-    final static String member_address = "AddressID;MemberID;StreetName;City;State;Postcode";
-    final static String cart = "CartID;CustID;ProductID;Quantity";
-    final static String order = "OrderID;DateTime;DeliveryAddressID;TotalPrice;Paid;Change";
-    final static String order_item = "ProductID;Quantity;OrderID";
+    final static String MEMBER_ADDRESS_HEADER = "AddressID;MemberID;StreetName;City;State;Postcode";
+    final static String CART_HEADER = "CartID;CustID;ProductID;Quantity";
+    final static String ORDER_HEADER = "OrderID;DateTime;DeliveryAddressID;TotalPrice;Paid;Change";
+    final static String ORDER_ITEM_HEADER = "ProductID;Quantity;OrderID";
 }
