ab4184df6e1dff0740f7b02cd172bb37dcba2991
Brrright <mingliang0524@gmail.com>
2023-02-18 00:52:34 +0800

    Updated file header, removed warehouse (extra feature)

--- src/main/java/com/mycompany/oodms/FileRelatedClass/FileHeader.java
+++ src/main/java/com/mycompany/oodms/FileRelatedClass/FileHeader.java
@@ -10,17 +10,22 @@ package com.mycompany.oodms.FileRelatedClass;
  */
 public class FileHeader {
     final static String ADMIN_HEADER = "AdminID;Admin Name;Admin Email;Password;Age;Gender;Phone Number;Picture";
-    final static String DELIVERY_STAFF_HEADER = "StaffID;Staff Name;Staff Email;Password;Age;Gender;State;Phone Number;Picture";
+    final static String DELIVERY_STAFF_HEADER = "StaffID;Staff Name;Staff Email;Password;Age;Gender;Phone Number;Picture";
     final static String MEMBER_HEARDER = "MemberID;Member Name;Member Email;Password;Age;Gender;Phone Number;Picture";
+    
+    final static String MEMBER_ADDRESS_HEADER = "AddressID;StreetName;City;State;Postcode;MemberID";
+    final static String CART_HEADER = "CartID;Quantity;ProductID;MemberID";
+    final static String DELIVERY_HEADER = "DeliveryID;Status;Date;AddressID;StaffID;MemberID;OrderID";
+    final static String ORDER_HEADER = "OrderID;DateTime;TotalPrice;Paid;Change;MemberID;AddressID";
+    final static String ORDER_ITEM_HEADER = "ProductID;Quantity;OrderID;MemberID";
 
     final static String PRODUCT_HEADER = "ProductID;Product Name;Price;Stock Quantity;Picture;CategoryID";
     final static String CATEGORY_HEADER = "CategoryID;CategoryName";
     
-    final static String WAREHOUSE_HEADER = "Name;Street Name;City;State;Postcode";
-    final static String DELIVERY_HEADER = "DeliveryID;Status;Date;AddressID";
+    // extra feature: warehouse
+//    final static String ORDER_ITEM_HEADER = "ProductID;Quantity;OrderID;MemberID;WarehouseID";
+//    final static String DELIVERY_HEADER = "DeliveryID;Status;Date;Postcode;StaffID;MemberID;WarehouseID";
+//    final static String DELIVERY_STAFF_HEADER = "StaffID;Staff Name;Staff Email;Password;Age;Gender;State;Phone Number;Picture;WarehouseID";
+//    final static String WAREHOUSE_HEADER = "WarehouseID;Name;Street Name;City;State;Postcode";
     
-    final static String MEMBER_ADDRESS_HEADER = "AddressID;MemberID;StreetName;City;State;Postcode";
-    final static String CART_HEADER = "CartID;CustID;ProductID;Quantity";
-    final static String ORDER_HEADER = "OrderID;DateTime;DeliveryAddressID;TotalPrice;Paid;Change";
-    final static String ORDER_ITEM_HEADER = "ProductID;Quantity;OrderID";
 }
