19437fa023097f7fce085cf5381f982824d29aa3
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-02-19 16:01:11 +0800

    change delivery's method name

--- src/main/java/com/mycompany/oodms/Delivery.java
+++ src/main/java/com/mycompany/oodms/Delivery.java
@@ -10,48 +10,48 @@ import java.time.LocalDateTime;
  *
  * @author User
  */
-
 public class Delivery {
+
     private int id;
     private Order order;
     private LocalDateTime date_time;
     private DeliveryStaff deliveryStaff;
     private DeliveryStatus status;
 
-  // constructor, getters, and setters
-    public Delivery(int id, Order order, LocalDateTime date_time, DeliveryStaff deliveryStaff, DeliveryStatus status){
+    // constructor, getters, and setters
+    public Delivery(int id, Order order, LocalDateTime date_time, DeliveryStaff deliveryStaff, DeliveryStatus status) {
         this.id = id;
         this.order = order;
         this.date_time = date_time;
         this.deliveryStaff = deliveryStaff;
         this.status = status;
     }
-    
-    public int getDeliverID(){
+
+    public int getDeliveryID() {
         return this.id;
     }
-    
-    public Order getOrder(){
+
+    public Order getOrder() {
         return this.order;
     }
-    
-    public void setOrder(Order order){
+
+    public void setOrder(Order order) {
         this.order = order;
     }
-    
+
     public DeliveryStaff getStaff() {
         return this.deliveryStaff;
     }
-    
-    public void setStaff(DeliveryStaff staff){
-       this.deliveryStaff = staff;
+
+    public void setStaff(DeliveryStaff staff) {
+        this.deliveryStaff = staff;
     }
-    
-    public DeliveryStatus getStatus(){
+
+    public DeliveryStatus getStatus() {
         return this.status;
     }
-    
-    public void setStatus(DeliveryStatus status){
+
+    public void setStatus(DeliveryStatus status) {
         this.status = status;
     }
 }
