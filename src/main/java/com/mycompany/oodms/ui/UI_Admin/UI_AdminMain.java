63f9c6d1b3b2bfb8d71484486a9aba10b6ce435c
chanhw <33367654+chanhw@users.noreply.github.com>
2023-02-20 20:12:30 +0800

    WIP UI_Admin main

---
+++ src/main/java/com/mycompany/oodms/ui/UI_Admin/UI_AdminMain.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oodms.ui.UI_Admin;
+
+import com.mycompany.oodms.ui.UI_Header;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author hw
+ */
+public class UI_AdminMain extends JPanel{
+    UI_Header header;
+    
+    JLabel title;
+    
+    JButton 
+    
+    
+    
+    public UI_AdminMain() {
+    
+    }
+    
+}
