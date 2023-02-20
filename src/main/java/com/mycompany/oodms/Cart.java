f9e4bab9c637de03b5ab3ad336fa200a6f446906
Brrright <mingliang0524@gmail.com>
2023-02-21 04:53:58 +0800

    Update Cart.java

--- src/main/java/com/mycompany/oodms/Cart.java
+++ src/main/java/com/mycompany/oodms/Cart.java
@@ -11,20 +11,37 @@ import java.util.ArrayList;
  * @author mingl
  */
 public class Cart {
-    private String cartID;
-    private ArrayList<Product> products;
+    private int cartID;
+    private Member member;
+    private ArrayList<CartItem> cart_items;
     
-    Cart(String id, Product product){
+    public Cart(int id,Member member){
         this.cartID = id;
-        this.products.add(product);
+        this.cart_items = new ArrayList<>();
+        this.member = member;
     }
     
-    
-    public String getCartID(){
+    public int getCartID(){
         return this.cartID;
     }
     
-    public ArrayList<Product> getCartProducts(){
-        return this.products;
+    public ArrayList<CartItem> getCartProducts(){
+        return this.cart_items;
     }
+    
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        for (CartItem item : cart_items) {
+            totalPrice += item.getTotal();
+        }
+        return totalPrice;
+    }
+    
+     public void addItem(CartItem item) {
+        cart_items.add(item);
+    }
+     
+//     public void removeItem(CartItem item) {
+//        cart_items.remove(item);
+//    }
 }
