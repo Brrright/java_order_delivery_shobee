fd6404fab12e79e9b59e9fe080731ba89746d4fe
chanhw <33367654+chanhw@users.noreply.github.com>
2023-02-21 23:03:09 +0800

    UI_AdminMain

--- src/main/java/com/mycompany/oodms/ui/UI_Admin/UI_AdminMain.java
+++ src/main/java/com/mycompany/oodms/ui/UI_Admin/UI_AdminMain.java
@@ -4,7 +4,15 @@
  */
 package com.mycompany.oodms.ui.UI_Admin;
 
+import static com.mycompany.oodms.OODMS_Main.frame;
 import com.mycompany.oodms.ui.UI_Header;
+import com.mycompany.oodms.ui.UI_Profile;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -13,16 +21,78 @@ import javax.swing.JPanel;
  * @author hw
  */
 public class UI_AdminMain extends JPanel{
-    UI_Header header;
+    UI_Header heading;
+    
     
     JLabel title;
     
-    JButton 
+    JButton generalManagementBtn;
+    ImageIcon generalManagementBtn_Icon = new ImageIcon("src/main/java/com/mycompany/oodms/ui/UI_Admin/pictures/generalManagementBtn.png");
     
-    
+    JButton deliveryManagementBtn;
+    ImageIcon deliverytBtn_Icon = new ImageIcon("src/main/java/com/mycompany/oodms/ui/UI_Admin/pictures/deliveryBtn.png");
+
     
     public UI_AdminMain() {
+        // heading
+        heading = new UI_Header();
+        heading.setBounds(0,0,1080,50);
+        
+        // JLabel - title (welcome, admin)
+        title = new JLabel("Welcome, admin");
+        title.setFont(new Font("MV Boli",Font.BOLD,37));
+        title.setBounds(382,200,312,37);
     
+        // JButton - general management button (direct to user, item, category management interface)
+        generalManagementBtn = new JButton(generalManagementBtn_Icon);
+        generalManagementBtn.setText("<html>user, item, category<br>management</html>");
+        generalManagementBtn.setFont(new Font("MV Boli",Font.PLAIN,12));
+        generalManagementBtn.setForeground(Color.GRAY);
+        generalManagementBtn.setHorizontalAlignment(JLabel.CENTER);
+        generalManagementBtn.setVerticalAlignment(JLabel.CENTER);
+        generalManagementBtn.setHorizontalTextPosition(JLabel.CENTER);
+        generalManagementBtn.setVerticalTextPosition(JLabel.BOTTOM);
+        generalManagementBtn.setIconTextGap(15);
+        generalManagementBtn.setBounds(324,300,183,235);
+        generalManagementBtn.setFocusable(false);
+        generalManagementBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        generalManagementBtn.setOpaque(false);
+        generalManagementBtn.setBorder(BorderFactory.createEmptyBorder()); 
+        generalManagementBtn.addActionListener(e -> {
+            frame.replacePanel(new UI_UserManagement());
+        });
+        
+        
+        // JButton - delivery management btn
+        deliveryManagementBtn = new JButton(deliverytBtn_Icon);
+        deliveryManagementBtn.setText("<html>delivery<br>management</html>");
+        deliveryManagementBtn.setFont(new Font("MV Boli",Font.PLAIN,12));
+        deliveryManagementBtn.setForeground(Color.GRAY);
+        deliveryManagementBtn.setHorizontalAlignment(JLabel.CENTER);
+        deliveryManagementBtn.setVerticalAlignment(JLabel.CENTER);
+        deliveryManagementBtn.setHorizontalTextPosition(JLabel.CENTER);
+        deliveryManagementBtn.setVerticalTextPosition(JLabel.BOTTOM);
+        deliveryManagementBtn.setIconTextGap(15);
+        deliveryManagementBtn.setBounds(592,300,165,235);
+        deliveryManagementBtn.setFocusable(false);
+        deliveryManagementBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        deliveryManagementBtn.setOpaque(false);
+        deliveryManagementBtn.setBorder(BorderFactory.createEmptyBorder()); 
+        deliveryManagementBtn.addActionListener(e -> {
+            // direct to general management interface
+        });
+        
+        
+        // this JPanel
+        this.setSize(1080, 768);
+        this.setBackground(Color.white);
+        this.setLayout(null);
+        
+        this.add(heading);
+        this.add(title);
+        
+        this.add(generalManagementBtn);
+        this.add(deliveryManagementBtn);
     }
     
 }
