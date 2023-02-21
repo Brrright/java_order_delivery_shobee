298619751bb566031c777e219729bca701db8c9f
Brrright <mingliang0524@gmail.com>
2023-02-21 12:47:07 +0800

    [Done] CartItemService

--- src/main/java/com/mycompany/oodms/Services/CartItemService.java
+++ src/main/java/com/mycompany/oodms/Services/CartItemService.java
@@ -5,6 +5,7 @@
 package com.mycompany.oodms.Services;
 
 
+import com.mycompany.oodms.Cart;
 import com.mycompany.oodms.Services.User.MemberService;
 import com.mycompany.oodms.CartItem;
 import com.mycompany.oodms.FileRelatedClass.FileHandler;
@@ -21,9 +22,9 @@ import java.util.List;
  */
 public class CartItemService {
     private ArrayList<CartItem> cart_items;
+    FileHandler cart_item_file = new FileHandler(FileName.CART_ITEM);
     
     public CartItemService(){
-            FileHandler cart_item_file = new FileHandler(FileName.CART_ITEM);
             List<FileRecord> item_records = cart_item_file.FetchRecord();
             item_records.forEach((record) -> {
             CartItem cart_item_object = convertToObject(record);
@@ -40,16 +41,25 @@ public class CartItemService {
         int qty = Integer.parseInt(cart_item_data[2]);
         int product_id = Integer.parseInt(cart_item_data[1]);
         int member_id = Integer.parseInt(cart_item_data[3]);
+        int cart_id = Integer.parseInt(cart_item_data[0]);
         
         MemberService member_service = new MemberService();
         Member member = member_service.getMember(member_id);
         
         ProductService product_service = new ProductService();
         Product product = product_service.getProduct(product_id);
-        return new CartItem(member, product, qty);
+        
+        CartService cart_service = new CartService();
+        Cart cart = cart_service.getCart(cart_id);
+        return new CartItem(cart, member, product, qty);
     }
     
-    public ArrayList<CartItem> getCartItems(){
+    private FileRecord convertToFileRecord(CartItem item){
+        String cart_item_record_string = item.getCart().getCartID()+ ";" + item.getProduct().getProductID() + ";" + item.getQuantity() + ";" + item.getMember().getID();
+        return new FileRecord(item.getCart().getCartID(), cart_item_record_string);
+    }
+    
+    public ArrayList<CartItem> getCartItems(int member_id){
         return this.cart_items;
     }
     
@@ -65,5 +75,33 @@ public class CartItemService {
             System.out.println("not such record in this \"cart_items\".  FIND A WAY TO HANDLE**");
         }
         return response;
+    }
+    
+    public void addCartItem(CartItem item){
+        this.cart_items.add(item);
+        FileRecord cart_item_record=  convertToFileRecord(item);
+        cart_item_file.InsertRecord(cart_item_record);
+    }
+    
+    public void updateCartItem(CartItem item){
+        for(int i=0; i < cart_items.size(); i++){
+            if(cart_items.get(i).getCart().getCartID() == item.getCart().getCartID() && cart_items.get(i).getProduct().getProductID() == item.getProduct().getProductID()){
+                cart_items.set(i, item);
+                FileRecord cart_item_record = convertToFileRecord(item);
+                cart_item_file.UpdateRecord(cart_item_record);
+                break;
+            }
+        }
+    }
+    
+    public void deleteCartItem(CartItem item){
+        for(int i=0; i < cart_items.size(); i++){
+            if(cart_items.get(i).getCart().getCartID() == item.getCart().getCartID() && cart_items.get(i).getProduct().getProductID() == item.getProduct().getProductID()){
+                cart_items.set(i, item);
+                FileRecord cart_item_record = convertToFileRecord(item);
+                cart_item_file.DeleteRecord(cart_item_record);
+                break;
+            }
+        }
     }
 }
