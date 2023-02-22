314ddfc6b9395328fce9b1a38981802b5f7ee812
chanhw <33367654+chanhw@users.noreply.github.com>
2023-02-22 20:13:10 +0800

    UI - Admin report

---
+++ src/main/java/com/mycompany/oodms/ui/UI_Admin/UI_ReportOrders.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oodms.ui.UI_Admin;
+
+/**
+ *
+ * @author hw
+ */
+public class UI_ReportOrders {
+    
+}
