1ae8987faea403dbf5cad878d27a03eb028d6f73
Ming Liang <83805050+Brrright@users.noreply.github.com>
2023-02-23 17:58:52 +0800

    FileName set Member address

--- src/main/java/com/mycompany/oodms/FileRelatedClass/FileName.java
+++ src/main/java/com/mycompany/oodms/FileRelatedClass/FileName.java
@@ -19,7 +19,7 @@ public class FileName {
     public static final String WAREHOUSE = "warehouse";
     public static final String DELIVERY = "delivery";
     
-    public static final String MEMBER_ADDRESS = "address";
+    public static final String MEMBER_ADDRESS = "member_address";
     public static final String CART = "cart";
     public static final String CART_ITEM = "cart_item";
     public static final String ORDER = "order";
